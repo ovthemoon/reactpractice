593bb276ff126e07e2aeec6a7b1e7cbaaec3cc81
ovthemoon <seungmoonchoi71@gmail.com>
2024-07-15 01:15:54 +0900

    Fix : Comment

--- reactpractice2/src/chapter_05/Comment.jsx
+++ reactpractice2/src/chapter_05/Comment.jsx
@@ -1,5 +1,38 @@
 import React from "react"
-
+const styles={
+    wrapper:{
+        margin :8,
+        padding:8,
+        display:"flex",
+        flexDirection:"row",
+        border:"1px solid gray",
+        borderRadius:16,
+    },
+    imageContainer:{
+     
+    },
+    image:{
+        width:100,
+        height:100,
+        borderRadius:50,
+    },
+    contentContainer:{
+        marginLeft:8,
+        display:"flex",
+        flexDirection:"column",
+        justifyContent:"center",
+    },
+    nameText:{
+        color:"black",
+        fontSize:16,
+        fontWeight:"bold",
+    },
+    commentText:{
+        color:"black",
+        fontSize:14,
+    },
+    
+}
 function Comment(props){
     return (
         <div>
